375ebd4af8bf8ae7aa173c8cbedd44bd651fe2e4
Sprof22 <richmondelaigwu@gmail.com>
2023-08-24 13:28:05 +0100

    space-station

---
+++ relay.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type RelayEndpoint struct {
+	DataChan chan DataPacket
+}
+
+func (re *RelayEndpoint) AcceptDataPacket(w http.ResponseWriter, r *http.Request) {
+	var packet DataPacket
+	err := json.NewDecoder(r.Body).Decode(&packet)
+	if err != nil {
+		http.Error(w, "Failed to decode data packet", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Received data packet: %+v\n", packet)
+
+	re.DataChan <- packet
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (re *RelayEndpoint) ProvideDataPacket(w http.ResponseWriter, r *http.Request) {
+	packet := <-re.DataChan
+
+	// Print the data packet before sending it
+	fmt.Printf("Providing data packet: %+v\n", packet)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(packet)
+}
